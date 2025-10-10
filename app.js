7c6deb0d6a8a31e124696c30ad99e2646bd560a7
manuel333m <manumendozao.602@gmail.com>
2025-10-10 15:29:51 -0400

    Ajustes

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+
+(async() => {
+try {
+    await mongoose.connect(process.env.MONGO_URI_TEST);
+    console.log('Conectado a Mongo DB');
+    
+} catch (error) {
+    console.log(error);
+    
+}
+})()
+
+module.exports = app;
