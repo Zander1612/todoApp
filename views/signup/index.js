07954e9323af399af550a420d629b637147f4d32
JRiveraCode <jhrva23@gmail.com>
2025-10-12 15:34:32 -0400

    add-signupAvance

---
+++ views/signup/index.js
@@ -0,0 +1,81 @@
+//Validation Patterns (Regex)
+const EMAIL_VALIDATION = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const PASSWORD_VALIDATION = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,16}$/;
+const NAME_VALIDATION = /^[a-zA-ZÀ-ÿ\s]{5,40}$/;
+
+//DOM Elements
+const form = document.querySelector('#form');
+const nameInput = document.querySelector('#name-input');
+const emailInput = document.querySelector('#email-input');
+const passwordInput = document.querySelector('#password-input');
+const matchInput = document.querySelector('#match-input');
+const formBtn = document.querySelector('#form-btn');
+
+
+//Validation
+let nameValidation = false;
+let emailValidation = false;
+let passwordValidation = false;
+let matchValidation = false;
+
+const validation = (input, regexValidation) => {
+
+    formBtn.disabled = nameValidation && emailValidation && passwordValidation && matchValidation ? false : true;
+
+    if (input.value === '') {
+        input.classList.remove('outline-red-700', 'outline-2', 'outline');
+        input.classList.remove('outline-green-700', 'outline-2', 'outline');
+        input.classList.add('focus:outline-indigo-700');
+    } else if (regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.add('outline-green-700', 'outline-2', 'outline');
+    } else if (!regexValidation) {
+        input.classList.remove('focus:outline-indigo-700');
+        input.classList.remove('outline-green-700', 'outline-2', 'outline');
+        input.classList.add('outline-red-700', 'outline-2', 'outline');
+    };
+}
+
+//Event Listener
+nameInput.addEventListener('input', e => {
+    nameValidation = NAME_VALIDATION.test(e.target.value);
+    validation(nameInput, nameValidation);  
+})
+
+emailInput.addEventListener('input', e => {
+    emailValidation = EMAIL_VALIDATION.test(e.target.value);
+    validation(emailInput, emailValidation);  
+})
+
+passwordInput.addEventListener('input', e => {
+    passwordValidation = PASSWORD_VALIDATION.test(e.target.value);
+    validation(passwordInput, passwordValidation);
+    matchValidation = (e.target.value === matchInput.value) && (matchInput.value !== '');
+    validation(matchInput, matchValidation);
+})
+
+matchInput.addEventListener('input', e => {
+    matchValidation = (e.target.value === passwordInput.value) && (e.target.value !== '');
+    validation(matchInput, matchValidation);
+})
+
+//Submit Form
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    try {
+        const newUser = {
+            name: nameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value
+        }
+        const response = await axios.post('/api/users', newUser);
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+})
+
+
