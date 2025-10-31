bf59f4d72cb22535b30468f8103e337da635fd2d
JRiveraCode <jhrva23@gmail.com>
2025-10-31 13:14:38 -0400

    update Todo add

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,17 +1,57 @@
-const todosRouter = require('express').Router();
+//Importanciones
+const todosRouter = require("express").Router();
 const User = require('../models/user');
-const Todo = require('../models/todo')
+const Todo = require('../models/todo');
 
-todosRouter.get('/', async  (req, res) => {
-    const user = req.user;
+
+//Obtener todas las tareas del usuario (GET /api/todos)
+todosRouter.get('/', async (request, response) => {
+    const user = request.user;
     const todos = await Todo.find({ user: user.id });
-    return res.status(200).json(todos);
+    return response.status(200).json(todos);
 });
 
-todosRouter.post('/', async  (req, res) => {
-    const user = req.user;
-    const {text } = req.body;
-    return res.status(200).json(todos);
+
+//Crear una nueva tarea (POST /api/todos
+todosRouter.post('/', async (request, response) => {
+    const user = request.user;
+    const { text } = request.body;
+    const newTodo = new Todo ({
+        text,
+        checked: false,
+        user: user._id
+    })
+    const savedTodo = await newTodo.save();
+    user.todos = user.todos.concat(savedTodo._id);
+    await user.save();
+
+    return response.status(201).json(savedTodo);
+});
+
+
+//Eliminar una tarea (DELETE /api/todos/:id)
+todosRouter.delete('/:id', async (request, response) => {
+    const user = request.user;
+    
+    await Todo.findByIdAndDelete(request.params.id)
+
+    user.todos = user.todos.filter(todo => todo.id !== request.params.id);
+
+    await user.save();
+    return response.sendStatus(204);
+});
+
+
+//Actualizar el estado de una tarea (PATCH /api/todos/:id)
+todosRouter.patch('/:id', async (request, response) => {
+    const user = request.user;
+    
+    const { checked } = request.body;
+    console.log(request.params.id);
+
+    await Todo.findByIdAndUpdate(request.params.id, { checked });
+
+    return response.sendStatus(200);
 });
 
 
